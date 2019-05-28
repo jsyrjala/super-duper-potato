e84bb348681ba6c29fe50d8018d3dd464bca6c2d
Juha Syrjälä <juha.syrjala@iki.fi>
2019-05-28 08:41:49 +0300

    Component updating

--- src/clojure_ces/system.clj
+++ src/clojure_ces/system.clj
@@ -45,26 +45,42 @@
         (-> result :system/entity) result
         :default {:system/entity old-entity}))
 
-(defn update-entity [world system entity-id]
-  (let [update-fn (-> system :system/update-fn)
-        entity (-> world :world/entities entity-id)]
-    (when entity
-      (print "Missing entity for id. Bug somewhere:" entity-id))
-    (update-fn world system entity)
+(defn update-entity [world system entity]
+  (let [update-fn (-> system :system/update-fn)]
+    (update-fn world system entity)))
+
+(defn update-entity-by-id [world system entity-id]
+  (println "update-entity" "id:" entity-id)
+  (let [entity (-> world :world/entities (get entity-id))]
+    (when (not entity)
+      (println "Missing entity for id. Bug somewhere:" entity-id))
+    (let [x (update-entity world system entity)]
+      (println "update-fn res" x)
+      x)
     ))
 
 (defn apply-system [world system]
   (let [entity-ids (-> system :system/entity-ids)
-        update-commands (map #(update-entity world system %) entity-ids)]
+        update-commands (map #(update-entity-by-id world system %) entity-ids)]
+    (println "update-comd" update-commands (nil? update-commands) entity-ids)
+    (cond (nil? update-commands) world
+          :default world)
     )
   )
 
 (defn game-loop [world]
-  (let [entities (-> :system/entities world)
-        new-world (reduce #(apply-system world %) (-> world :world/systems))]
-    nil))
+  (let [systems (-> world :world/systems)
+        _ (println "game-loop systems" (count systems))
+        ;; TODO
+        new-world (map #(apply-system world %) (-> world :world/systems))
+        ;;new-world (reduce #(apply-system world %) (-> world :world/systems))
+        ]
+    new-world))
 
 (defn- add-entities-to-system [system entities]
+  ;; TODO adding entities to system is not good, they should not be part of a system?
+  ;;      makes testing systems difficult
+  ;;
   ;; TODO if entities contains dupes, they are not handled correctly
   ;; if system already contains entity, it is filtered correctly
   (let [applies? (-> system :system/applies-fn)
@@ -110,19 +126,39 @@
    }
   )
 
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defn update-position [world system entity]
-  (let [pos (:position/location entity)
-        [x y] [(:x pos) (:y pos)]
-        new-pos (assoc pos :x (inc x) :y (inc y))]
-    (update-in entity [:position/location] new-pos)))
+(defn component-updater [component applies? update-fn]
+  (if (applies? component)
+    (update-fn component)
+    component))
 
+(defn for-component-type [type]
+  (fn [component]
+    (= (:component/type component) type)))
+
+(defn update-component-with [component-type update-fn]
+  (fn [world system entity]
+    (let [components (-> entity :entity/components)
+          component-update-fn (fn [component]
+                                (update-fn world system entity component))
+          new-components (map #(component-updater %
+                                                  (for-component-type component-type)
+                                                  component-update-fn)
+                              components)]
+      (assoc entity :entity/components new-components))))
+
+(defn position-update [world system entity component]
+  (let [pos (:position/location component)
+        [x y] [(:x pos) (:y pos)]]
+    (assoc position :position/location {:x (inc x) :y (inc y)})))
+
+(def update-position (update-component-with :position position-update))
 
 (def moving-system (create-system "Moving"
                                   update-position
-                                  (create-applies? [:position]))
-  )
+                                  (create-applies? [:position])))
 
 (def drawable-system (create-system "Drawable"
                                   update-position
@@ -145,3 +181,4 @@
 
 (def world (create-world [moving-system drawable-system]))
 
+(def world2 (-> world (add-entities [entity1]) (add-entities [entity1 entity2])))
