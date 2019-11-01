7b8c9b4e537db77158cf38cfe09d87f77c25c841
Juha Syrjälä <juha.syrjala@iki.fi>
2019-11-01 17:25:45 +0200

    Something visible on the screen

---
+++ src/clojure_ces/example/litiengine.clj
@@ -0,0 +1,49 @@
+(ns clojure-ces.example.litiengine
+  (:require [clojure.tools.logging :as log])
+  (:import (de.gurkenlabs.litiengine Game GameListener)
+           (de.gurkenlabs.litiengine.gui.screens Screen Resolution)
+           (java.awt Graphics2D Color)
+           (de.gurkenlabs.litiengine.graphics RenderEngine)
+           )
+  (:gen-class))
+
+;; https://stackoverflow.com/questions/38573470/make-a-class-that-extends-a-class-with-overriding-in-clojure
+(defn make-screen [name render-fn]
+  (proxy [Screen]
+         [name]
+    (render [^Graphics2D g]
+      (render-fn g))))
+
+
+(defn draw-something [^Graphics2D g]
+  (let [x 5.0
+        y 10.0]
+    (.setColor g Color/RED)
+    (RenderEngine/renderText g "my text" x y)))
+
+(defn start-engine [args]
+  (log/info "start" args)
+  (Game/init (into-array String args))
+  (let [window (Game/window)]
+    (.setTitle window "Perkele!")
+    (.setResolution window (Resolution/custom 400 400 "4x3")))
+
+  (log/info "init done")
+  (Game/addGameListener (reify GameListener
+                          (started [_]
+                            (log/info "Gamelistener started"))
+                          (terminated [_]
+                            (log/info "Gamelistener terminated"))
+                          ))
+  (let [screen (make-screen "hii" draw-something)]
+    (.add (Game/screens) screen))
+
+  (Game/start)
+  (log/info "start done")
+  )
+
+(defn -main [& args]
+  (log/info "Starting")
+  (start-engine args)
+  (log/info "Game loop running")
+  )
