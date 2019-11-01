b505b66755b1b1219b041edbd66bd8091724b9f0
Juha Syrjälä <juha.syrjala@iki.fi>
2019-11-01 21:14:23 +0200

    Fix slow updated when out of focus

--- src/clojure_ces/example/graphics.clj
+++ src/clojure_ces/example/graphics.clj
@@ -1,13 +1,12 @@
 (ns clojure-ces.example.graphics
   (:require [clojure-ces.system :as system])
-  (:import (java.awt Graphics2D Color)
+  (:import (java.awt Graphics2D Color Toolkit)
            (de.gurkenlabs.litiengine.graphics RenderEngine)))
 
 (def text (atom "foo2"))
 
 (defn draw-things [^Graphics2D g world]
-  (let [w @world
-        entities (:world/entities w)
+  (let [entities (:world/entities @world)
         id (first (keys entities))
         entity-1 (entities id)
         position (system/first-component entity-1 :position)
@@ -15,8 +14,11 @@
         ;; x 0.0
         ;;y 10.0
         ]
+    ;; TODO this is totally in wrong place?
     (swap! world system/game-loop)
     (.setColor g Color/RED)
     (RenderEngine/renderText g (str @text " bar") x y)
+    ;; https://stackoverflow.com/questions/18684220/why-is-java-application-running-smoother-when-moving-mouse-over-it-video-includ
+    (.sync (Toolkit/getDefaultToolkit))
     ))
 
